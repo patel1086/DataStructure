e24feda16801dfbf81848fdfc54dea15a4236a02
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-12 22:38:52 +0530

    [Jitendra]Added: Implemented BST for more than 3 numbers

--- src/test/java/com/bridgelabz/linkedList/MyBinaryTreeTest.java
+++ src/test/java/com/bridgelabz/linkedList/MyBinaryTreeTest.java
@@ -5,13 +5,31 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyBinaryTreeTest {
+	MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
 	@Test
 	public void given3NumbersWhenAddedToBinarySearchTreeShouldReturnSizeIsTrue() {
-		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        int sizeOfBST = binaryTree.getSize();
+        Assert.assertEquals(3, sizeOfBST);
+	}
+	@Test
+    public void givenManyNumbers_whenAddedToBST_shouldReturnHeightOfTree() {
         binaryTree.add(56);
         binaryTree.add(30);
         binaryTree.add(70);
-        int sizeOfBinaryTree = binaryTree.getSize();
-        Assert.assertEquals(3, sizeOfBinaryTree);
-	}
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        int sizeOfBST = binaryTree.getSize();
+        Assert.assertEquals(13, sizeOfBST);
+    }
 }
