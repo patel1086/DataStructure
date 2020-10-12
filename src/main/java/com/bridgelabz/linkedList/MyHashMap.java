4c1f404037e8d8c77bf1fbcb6845e84a7f890dfe
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-12 16:27:49 +0530

    [Jitendra]Added: Get frequrncy of word in Paragraph using HashMap with LinkedList

--- src/main/java/com/bridgelabz/linkedList/MyHashMap.java
+++ src/main/java/com/bridgelabz/linkedList/MyHashMap.java
@@ -6,15 +6,14 @@ public class MyHashMap<K,V>{
 	public MyHashMap() {
 		this.myLinkedList=new MyLinkedList<>();
 	}
-	
+	//Method for getting value for a particular key
 	public V get(K key) {
 		MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)this.myLinkedList.search(key);
 		return (myMapNode==null)?null:myMapNode.getValue();
 	}
+	//Method for add key value pair in HashMap
 	public void add(K key,V value) {
 		MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)this.myLinkedList.search(key);
-		//MyMapNode<K,V> myMapNode;
-		
 		if(myMapNode==null) {
 			myMapNode=new MyMapNode<>(key,value);
 			this.myLinkedList.addElement(myMapNode);
--- src/test/java/com/bridgelabz/linkedList/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/linkedList/MyHashMapTest.java
@@ -19,5 +19,24 @@ public class MyHashMapTest{
 		int frequency=myHashMap.get("to");
 		System.out.println(myHashMap);
 		Assert.assertEquals(2,frequency);
+	}
+	@Test
+	public void givenALongParagraph_WhenWordsAreAddedToList_ShouldReturnWordOfFrequency() {
+		String sentence="Paranoids are not\r\n"
+				+ "paranoid because they are paranoid but\r\n"
+				+ "because they keep putting themselves\r\n"
+				+ "deliberately into paranoid avoidable\r\n"
+				+ "situations";
+		MyHashMap<String,Integer> myHashMap=new MyHashMap<>();
+		String[] words=sentence.toLowerCase().split(" ");
+		for(String word:words) {
+			Integer value=myHashMap.get(word);
+			if(value==null) value=1;
+			else value=value+1;
+			myHashMap.add(word, value);
 		}
+		int frequency=myHashMap.get("they");
+		System.out.println(myHashMap);
+		Assert.assertEquals(2,frequency);
+	}
 }
